ca23188fb97acbb1ecba764f25700a3edc73a0ed
Anurag <anurec9119@gmail.com>
2021-11-15 22:03:44 +0530

    comm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Video from "./Video/bgvideo.mp4";
+import './box.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video
+        autoPlay
+        loop
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "0px",
+          top: "0px",
+          height: "100%",
+          ojectFit: "cover",
+          transform: "translate(-50%,-50%,)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={Video} type="video/mp4" />
+      </video>
+      <div>
+        <span className="L">
+          <p>
+            LUXURY
+            <br />
+          </p>
+        </span>
+        <h3 className="o">of</h3>
+        <span className="D">DESIGN</span>
+        <img src="image/Robo-1.gif" className="clsfirstgif"></img>
+
+        <div className="subhead">
+          <h1>
+            DESIGNS THAT MAKE <br /> A DIFFERENCE
+          </h1>
+        </div>
+        <img src="image/Robo-2.gif" className="clsSecgif"></img>
+      </div>
     </div>
   );
 }
---
+++ src/box.js
@@ -0,0 +1,14 @@
+import  React from "react";
+import Box from "@mui/material/Box";
+
+function BoxSx() {
+
+    return(
+        <div className="box">
+            <Box clone style={{color:'white'}} m={3}> 
+              <h1>Anurag Chaursia</h1>
+            </Box>
+        </div>
+    )
+}
+export default BoxSx;
